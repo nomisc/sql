7ae1d2b66d2440b214ea9864a120f01d391373a2
Simon <simon.ciperle@pirnar.si>
2019-10-16 14:22:12 +0200

    simple builid test

--- test.sql
+++ test.sql
@@ -1,4 +1,7 @@
-select
-  *
-from
-  REPORTING_CONTRACT_LAST
+
+create or replace function api2._test_call(input_param numeric) 
+returns boolean as  $$	
+begin  		
+	return _return_status; 
+end;  $$
+language plpgsql;
